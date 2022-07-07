49c468e261938ddb2b82e4e2ce1e2de9dfcd6cf4
mper.28 <ramadhan.feriansyah28@gmail.com>
2022-07-07 22:12:12 +0700

    ini adalah tugas 11 lanjutan di1 file githubt

---
+++ lib/app/utils/style/AppColors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryBg = Color(0xFF808080);
+  static const primaryText = Color(0xFFFFDAD0);
+  static const cardBg = Colorc(0xFFBFE2FF);
+}
